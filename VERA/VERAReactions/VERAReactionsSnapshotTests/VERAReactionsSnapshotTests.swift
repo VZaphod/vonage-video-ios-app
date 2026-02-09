19745d38bcc69c6b8d47c3e167650680e8ebf5b8
jorgesanmartin-vng <jorge.sanmartin@vonage.com>
2026-02-09 15:41:56 +0100

    VIDSOL-464: Create Reactions module (#83)

---
+++ VERA/VERAReactions/VERAReactionsSnapshotTests/VERAReactionsSnapshotTests.swift
@@ -0,0 +1,10 @@
+//
+//  Created by Vonage on 2/9/26.
+//
+
+import SnapshotTesting
+import Testing
+
+struct VERAReactionsSnapshotTests {
+    // Add snapshot tests here
+}
---
+++ VERA/VERAReactions/VERAReactionsTests/VERAReactionsTests.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Vonage on 2/9/26.
+//
+
+import Testing
+import VERAReactions
+
+struct VERAReactionsTests {
+
+    @Test
+    func version() {
+        #expect(VERAReactions.version == "1.0.0")
+    }
+}
